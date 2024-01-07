9cefc184dd9ffc9f75f15e692a60792edd7de37d
NILANJANA DEY <nilanjanadey276@gmail.com>
2024-01-08 00:31:32 +0530

    update

--- Expense_tracker/settings.py
+++ Expense_tracker/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Expense_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
