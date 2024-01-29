ae0f51f48a94b5191cd7b6d182f34b9f366ee244
NILANJANA DEY <nilanjanadey276@gmail.com>
2024-01-29 14:17:20 +0530

    styling signin page

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def menu(request):
     return render(request,"Dashboard/dashboard.html")
+
+def calendar(request):
+    return render(request,"CALENDAR/")
