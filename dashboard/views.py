c712f1baa33e7f66ea7dda377c64bae9c52eec1d
NILANJANA DEY <nilanjanadey276@gmail.com>
2024-01-29 14:39:00 +0530

    linking the calender

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,4 +5,4 @@ def menu(request):
     return render(request,"Dashboard/dashboard.html")
 
 def calendar(request):
-    return render(request,"CALENDAR/")
+    return render(request,"CALENDER/calender.html")
